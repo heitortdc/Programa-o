dbd5c94523ea7c67007da281561a6a8c0064dc2a
heitortdc <heitorcavalcante@ua.pt>
2026-09-22 19:47:21 +0100

    Add sprint 1

---
+++ Sprints/sprint1.c
@@ -0,0 +1,15 @@
+int main(){
+int x;
+float temp;
+
+prrintf("digite um valor entre 0 e 1023");
+scanf("%d",x);
+
+temp=260*x/1023-20;
+
+printf("o valor digitado é %d ",x);
+
+printf("o resultado é %.2f ",temp);
+
+}
+
